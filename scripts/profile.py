05f0d320c71550cb47d84aa5ae81f766173f1535
jacquelinlai <lsrislucky@gmail.com>
2023-11-20 19:28:21 -0600

    Updated profile.py

---
+++ scripts/profile.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df_red = pd.read_csv('data/winequality-red.csv')
+df_wht =  pd.read_csv('data/winequality-white.csv')
+
