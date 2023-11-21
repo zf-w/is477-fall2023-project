1353c2da4df171ab0d4372f095642f1d16b377fc
jacquelinlai <lsrislucky@gmail.com>
2023-11-20 20:50:04 -0600

    export the combined dataframe to csv file

--- scripts/profile.py
+++ scripts/profile.py
@@ -1,8 +1,18 @@
 import numpy as np
+import os
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+from ydata_profiling import ProfileReport
 
 df_red = pd.read_csv('data/winequality-red.csv')
 df_wht =  pd.read_csv('data/winequality-white.csv')
 
+df = pd.concat([df_red, df_wht])
+
+if os.path.exists('data/winequality.csv'):
+    os.remove('data/winequality.csv')
+df.to_csv('data/winequality.csv')
+
+profile = ProfileReport(df, title="Profiling Report")
+if os.path.exists('profiling/report.html'):
+    os.remove('profiling/report.html')
+    profile.to_file("profiling/report.html")
